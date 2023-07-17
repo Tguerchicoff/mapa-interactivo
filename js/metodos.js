f07ae2e9f533b0064f03c4108b66e4c642c67c71
TOM4S <tomyamigos@gmail.com>
2023-07-16 22:55:17 -0300

    NUEVA API
    NEW API

--- js/metodos.js
+++ js/metodos.js
@@ -1,3 +1,6 @@
+const WEATHER_API_KEY = "00414df992944f56877202743230307";
+
+
 
 const colorScale = d3.scaleSequential().domain([40, 0]).interpolator(d3.interpolateRdYlBu);
 const paths = document.querySelectorAll('path');
@@ -8,21 +11,19 @@ addProvinceClickHandlers(paths);
 
 function updatePaths() {
   const paths = d3.selectAll('path');
-
   // Mostrar pantalla de carga
-  const loading = document.getElementById('loading');
   showLoading()
 
   paths.each(function() {
     const provinceTitle = this.getAttribute('title');
-    const apiKey = "c2eb58a53bf66a86592083b4cdb9d677";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${provinceTitle},USA&appid=${apiKey}&units=imperial`;
+    const apiKey = atob("YzJlYjU4YTUzYmY2NmE4NjU5MjA4M2I0Y2RiOWQ2Nzc=");
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=00414df992944f56877202743230307&q=${provinceTitle}&aqi=no&lang=es`;
 
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        if (data.main && data.main.temp) {
-          const temperature = ((data.main.temp - 32) * (5/9)).toFixed(1);
+        if (data.current && data.current.temp_c) {
+          const temperature = data.current.temp_c;
           // Choose a color based on temperature
           const color = colorScale(temperature);
           d3.select(this).attr('fill', color);
@@ -42,51 +43,62 @@ function updatePaths() {
 
 
 
-
-
 function addProvinceClickHandlers(paths) {
   paths.forEach(path => {
     path.addEventListener('click', onProvinceClick);
   })}
 
 
-function onProvinceClick(event) {
-  const provinceTitle = event.target.getAttribute('title');
-  const apiKey = atob("YzJlYjU4YTUzYmY2NmE4NjU5MjA4M2I0Y2RiOWQ2Nzc=");
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${provinceTitle},USA&appid=${apiKey}&units=imperial&lang=es`;
-
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      if (data.main && data.main.temp) {
-        const temperature = ((data.main.temp - 32) * (5/9)).toFixed(1);
-        const description = data.weather[0].description;
-        const iconCode = data.weather[0].icon;
-        const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
-        const message = `The current temperature in ${provinceTitle} is ${temperature}°C with ${description}.`;
-        showModal(`${provinceTitle}`,`${temperature}`,`${description}`,`${iconUrl}`)
-      } else {
-        console.error("Cannot find temperature data in response:", data);
-      }
-    })
-    .catch(error => console.error(error));
-}
-
-function showModal(provinceTitle, temperature, description, iconCode) {
-  const modal = new bootstrap.Modal(document.getElementById("modal-clima"));
-  const provinceName = document.getElementById("province-name");
-  const temp = document.getElementById("temperature");
-  const desc = document.getElementById("description");
-  const icon = document.getElementById("weather-icon");
+  function onProvinceClick(event) {
+    const provinceTitle = event.target.getAttribute('title');
+    const apiKey = atob("YzJlYjU4YTUzYmY2NmE4NjU5MjA4M2I0Y2RiOWQ2Nzc=");
+    const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=00414df992944f56877202743230307&q=${provinceTitle}&days=4&aqi=no&alerts=no&lang=es`;
   
-  provinceName.innerHTML = provinceTitle;
-  temp.innerHTML = `${temperature} °C`;
-  desc.innerHTML = description;
-  icon.src = iconCode;
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const currentWeather = data.current;
+        const forecast = data.forecast.forecastday.slice(1, 4); // Obtener los próximos 3 días
+  
+        if (currentWeather && currentWeather.temp_c) {
+          const temperature = currentWeather.temp_c.toFixed(1);
+          const description = currentWeather.condition.text;
+          const iconCode = currentWeather.condition.icon;
+          const iconUrl = `https:${iconCode}`;
+  
+          showModal(provinceTitle, temperature, description, iconUrl, forecast);
+        } else {
+          console.error("Cannot find current weather data in response:", data);
+        }
+      })
+      .catch(error => console.error(error));
+  }
 
-  modal.show();
-}
 
+  function showModal(province, currentTemperature, description, iconUrl) {
+    // Obtener el modal y los elementos del modal
+    const modalTitle = document.getElementById("province-name");
+    const temperatureElement = document.getElementById("temperature");
+    const descriptionElement = document.getElementById("description");
+    const iconElement = document.getElementById("weather-icon");
+  
+    // Configurar el título del modal
+    modalTitle.textContent = province;
+  
+    // Configurar la temperatura actual
+    temperatureElement.textContent = `${currentTemperature} °C`;
+  
+    // Configurar la descripción del clima
+    descriptionElement.textContent = description;
+  
+    // Configurar el icono del clima
+    iconElement.src = iconUrl;
+  
+    // Mostrar el modal
+    const modal = new bootstrap.Modal(document.getElementById("modal-clima"));
+    modal.show();
+  }
+  
 
 function showLoading() {
   const loadingContainer = document.getElementById('loading-container');
@@ -97,3 +109,5 @@ function hideLoading() {
   const loadingContainer = document.getElementById('loading-container');
   loadingContainer.style.display = 'none';
 }
+
+
